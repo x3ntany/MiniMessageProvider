1e2caf88e72d187ccdfa27a7e24d48234553f7eb
x3ntany <xentany@proton.me>
2025-03-24 04:16:20 +0900

    Initial commit

--- src/main/java/me/xentany/minimessageprovider/MiniMessageProvider.java
+++ src/main/java/me/xentany/minimessageprovider/MiniMessageProvider.java
@@ -2,6 +2,7 @@ package me.xentany.minimessageprovider;
 
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
+@SuppressWarnings("unused") //api
 public interface MiniMessageProvider {
 
   static MiniMessage get() {
